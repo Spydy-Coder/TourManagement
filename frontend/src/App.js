8226d152449ffb0840b4a19af1caafed566572f4
AnshumanSaini <90978443+AnshumanSaini@users.noreply.github.com>
2024-04-03 01:08:04 +0530

    made the login page.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,12 +3,14 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import AdminPortal from "./pages/AdminPortal";
 
 import Home from "./pages/Home";
+import Login from "./components/Login";
 
 function App() {
   return(
     <Router>
       <Routes>
         <Route  path="/" element={<Home/>}/>
+        <Route  path="/login" element={<Login/>}/>
         <Route  path="/admin/dashboard" element={<AdminPortal/>}/>
       </Routes>
 
--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,27 +1,101 @@
-import React from 'react';
-import "./Navbar.css" 
+import React from "react";
+import "./Navbar.css";
+import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { googleLogout } from "@react-oauth/google";
 
 export default function Navbar() {
+  const navigate  = useNavigate();
+  const [userData, setUserData] = useState(null);
+  useEffect(() => {
+    let currUser = localStorage.getItem("token");
+    // console.log(currUser);
+
+    //Making API request to fetch user using token
+    const fetchData = async () => {
+      const response = await fetch("http://localhost:8080/api/auth/getuser", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          token: currUser,
+        },
+      });
+      // console.log(response);
+      const contentType = response.headers.get("content-type");
+      if ((!contentType || !contentType.includes("application/json")) && !response.ok) {
+        const json = await response.json();
+        setUserData(json);
+        return;
+      }
+      const json = await response.json();
+      console.log(json);
+      setUserData(json);
+    };
+    fetchData();
+  }, []);
+
+  const logout = () => {
+    googleLogout();
+    localStorage.clear();
+    navigate("/");
+  };
+
+
   return (
     <nav className="navbar navbar-expand-lg ">
       <div className="container-fluid">
-        <a className="navbar-brand brand-name fs-1" href="/">Safar</a>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <a className="navbar-brand brand-name fs-1" href="/">
+          Safar
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNavAltMarkup"
+          aria-controls="navbarNavAltMarkup"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="collapse navbar-collapse justify-content-end " id="navbarNavAltMarkup">
+        <div
+          className="collapse navbar-collapse justify-content-end "
+          id="navbarNavAltMarkup"
+        >
           <div className="navbar-nav">
-            <a className="nav-link active fs-5 me-2" aria-current="page" href="/">Home</a>
-            <a className="nav-link fs-5 me-2" href="#">About</a>
-            <a className="nav-link fs-5 me-2" href="#">Bookings</a>
-            <a className="nav-link fs-5 me-4" href="#footer">Contact</a>
-            
+            <a
+              className="nav-link active fs-5 me-2"
+              aria-current="page"
+              href="/"
+            >
+              Home
+            </a>
+            <a className="nav-link fs-5 me-2" href="#">
+              About
+            </a>
+            <a className="nav-link fs-5 me-2" href="#">
+              Bookings
+            </a>
+            <a className="nav-link fs-5 me-4" href="#footer">
+              Contact
+            </a>
           </div>
-          <button className="btn  me-2 nav-button" type="button">User</button>
-          <a href="/admin/dashboard">
-          <button className="btn  me-2 nav-button" type="button">Admin</button>
-          </a>
-            
+          <button className="btn  me-2 nav-button" type="button">
+            User
+          </button>
+          
+          {(localStorage.getItem("token"))===null ? (
+            <a href="/login">
+              <button className="btn  me-2 nav-button" type="button">
+                Admin
+              </button>
+            </a>
+          ) : (
+              <button className="btn  me-2 nav-button" type="button" onClick={()=>{logout()}}>
+                Logout
+              </button>
+          )}
         </div>
       </div>
     </nav>
