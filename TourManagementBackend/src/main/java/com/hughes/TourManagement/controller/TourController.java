88db834448deb8a8aede0a414383190bf563581d
AnshumanSaini <90978443+AnshumanSaini@users.noreply.github.com>
2024-04-02 15:11:00 +0530

    Merge changes from master branch

--- TourManagementBackend/src/main/java/com/hughes/TourManagement/controller/TourController.java
+++ TourManagementBackend/src/main/java/com/hughes/TourManagement/controller/TourController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +18,7 @@ import com.hughes.TourManagement.model.Tour;
 import com.hughes.TourManagement.service.TourService;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000/")
 @RequestMapping("/api/pin")	
 public class TourController {
 	
---
+++ TourManagementBackend/src/main/java/com/hughes/TourManagement/controller/UserController.java
@@ -0,0 +1,33 @@
+package com.hughes.TourManagement.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.hughes.TourManagement.model.User;
+import com.hughes.TourManagement.service.UserService;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:3000/")
+@RequestMapping("/api/auth")	
+public class UserController {
+	
+	@Autowired
+	private UserService user_serv;
+	
+	@RequestMapping(value = "/login", method = RequestMethod.POST)
+	public String login(@RequestBody User user) {
+		return user_serv.login(user);
+	}
+	
+	@RequestMapping(value = "/getuser", method = RequestMethod.GET)
+	public User getUser(@RequestHeader("token") String token) {
+		System.out.println(token);
+		return user_serv.getUser(token);
+	}
+
+}
